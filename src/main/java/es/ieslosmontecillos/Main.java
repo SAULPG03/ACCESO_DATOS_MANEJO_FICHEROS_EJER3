3e4b115f16fd209ae7d877211d961f8e51bac823
Saúl Portales García <sporgar2706@g.educaand.es>
2024-11-26 12:03:05 +0100

    Avanzar por la parte 5

--- src/main/java/es/ieslosmontecillos/Main.java
+++ src/main/java/es/ieslosmontecillos/Main.java
@@ -1,19 +1,93 @@
 package es.ieslosmontecillos;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int opcion = 0;
+        File directorioPadre = new File("C:\\Users\\usuario\\IdeaProjects\\ACCESO_DATOS_MANEJO_FICHEROS_EJER3\\src\\main\\resources");
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("-----------MENU-----------");
+            System.out.println("1. Crear Directorio");
+            System.out.println("2. Crear Archivo");
+            System.out.println("3. Listar Directorio o Archivo");
+            System.out.println("4. Cambiar nombre a Directorio o Fichero");
+            System.out.println("5. Borrar Fichero o Directorio");
+            System.out.println("6. Insertar cadena de texto en fichero");
+            System.out.println("7. Añadir cadena de texto en fichero");
+            System.out.println("8. Salir");
+            System.out.print("Opcion: ");
+            opcion = sc.nextInt();
+            sc.nextLine();//Clean buffer keyboard
+            switch(opcion){
+                case 1:
+                    System.out.println("Ingrese el nombre del directorio");
+                    String nombre = sc.nextLine();
+                    newDirectory(directorioPadre,nombre);
+                    break;
+                case 2:
+                    System.out.println("Ingrese el nombre del archivo");
+                    String nombreArchivo = sc.nextLine();
+                    try {
+                        newFile(directorioPadre,nombreArchivo);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Ingrese la ruta del fichero o directorio a listar: ");
+                    String ruta = sc.nextLine();
+                    listDirectoyandFile(ruta);
+                    break;
+                case 4:
+                    System.out.println("Ingrese la ruta del nombre del fichero o archivo que quieres renombrar: ");
+                    String fileRename = sc.nextLine();
+                    renameFileorDirectory(fileRename,sc);
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    System.out.println("Te has salido del programa");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+        }while (opcion!=8);
+        sc.close();
     }
-    public static void newDirectory(File ruta){
-        ruta.mkdir();
+    public static void newDirectory(File ruta,String nombre){
+        String rutaPadre=ruta.getAbsolutePath();
+        File newDirectory=new File(rutaPadre,nombre);
+        boolean success=newDirectory.mkdir();
+        if(success){
+            System.out.println("El directorio "+nombre+" ha sido creado con exito");
+        }else{
+            System.out.println("El directorio "+nombre+" no se puede ser creado");
+        }
+
     }
-    public static void newFile(File ruta) throws IOException {
-        ruta.createNewFile();
+    public static void newFile(File ruta,String nombre) throws IOException {
+        String rutaPadre=ruta.getAbsolutePath();
+        File newFile=new File(rutaPadre,nombre);
+        boolean success=newFile.createNewFile();
+        if(success){
+            System.out.println("El fichero "+nombre+" ha sido creado con exito");
+        }else{
+            System.out.println("El fichero "+nombre+" no se puede ser creado");
+        }
     }
-    public static void listDirectoyandFile(File ruta){
+    public static void listDirectoyandFile(String path){
+        File ruta=new File(path);
         if(ruta.exists()){
             if(ruta.isDirectory()){
                 String[] files = ruta.list();
@@ -24,6 +98,71 @@ public class Main {
             else if(ruta.isFile()){
                 System.out.println(ruta);
             }
+        }else{
+            System.err.println("No se encuentra el fichero o directorio");
+        }
+    }
+
+    public static void renameFileorDirectory(String name,Scanner sc){
+        File ruta=new File(name);
+        String newName;
+        if(ruta.exists()){
+            if(ruta.isDirectory()){
+                System.out.println("Introduce el nuevo nombre: ");
+                newName = sc.nextLine();
+
+                if(ruta.renameTo(new File(ruta.getAbsolutePath().replace(ruta.getName(),newName)))){
+                    System.out.println("Se ha cambiado el nombre al Direcorio");
+                }else{
+                    System.out.println("No se ha cambiado el nombre al Direcorio");
+                }
+            } else if (ruta.isFile()) {
+                System.out.println("Introduce el nuevo nombre: ");
+                newName = sc.nextLine();
+                if(ruta.renameTo(new File(ruta.getAbsolutePath().replace(ruta.getName(),newName)))){
+                    System.out.println("Se ha cambiado el nombre al Fichero");
+                }else{
+                    System.out.println("No se ha cambiado el nombre al Fichero");
+                }
+            }
+        }else{
+            System.err.println("No se encuentra el fichero o directorio");
+        }
+    }
+
+    public static void deleteFileorDirectory(File ruta){
+        if(ruta.exists()){
+            ruta.delete();
+        }else{
+            System.err.println("No se encuentra el fichero o directorio");
+        }
+    }
+
+    public static void insertStringFile(File ruta,String string) throws IOException {
+        if (ruta.exists()) {
+            if (ruta.isFile()) {
+                FileWriter fw = new FileWriter(ruta);
+                fw.write(string);
+                fw.close();
+            } else if (ruta.isDirectory()) {
+                System.err.println("No se escribir en un directorio");
+            }
+        }else{
+            System.err.println("No se encuentra el fichero o directorio");
+        }
+    }
+
+    public static void appendStringFile(File ruta,String string) throws IOException {
+        if (ruta.exists()) {
+            if (ruta.isFile()) {
+                FileWriter fw = new FileWriter(ruta, true);
+                fw.write(string);
+                fw.close();
+            } else if (ruta.isDirectory()) {
+                System.err.println("No se escribir en un directorio");
+            }
+        }else{
+            System.err.println("No se encuentra el fichero o directorio");
         }
     }
 
